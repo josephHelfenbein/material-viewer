b7a3888de6f40e03fda855293d5e25d04205459e
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-24 17:54:44 -0500

    glm submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <glfw/include/GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
 #include <vector>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
