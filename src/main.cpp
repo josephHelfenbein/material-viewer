699b39bf958e575c0cb67f99b7082c530e111d06
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-24 17:50:14 -0500

    remove glm and add submodule for glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <glfw/include/GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
