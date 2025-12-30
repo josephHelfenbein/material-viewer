ff200d1a9dd87d0adf0dde7f3807551f5e45f323
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-12-29 19:55:41 -0500

    fix for linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm/glm.hpp>
-#include <glm/glm/gtc/matrix_transform.hpp>
-#include <glm/glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <vector>
 #include <memory>
 #define STB_IMAGE_IMPLEMENTATION
